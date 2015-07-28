643e7dee5820f9939ac937bd0d0def145b2c656d
Latoya Hall <latoya.cherice@gmail.com>
2015-07-27 19:44:44 -0700

    Commit Summary
    Fixed Js to get keydown and key press functions to work.

--- js/app.js
+++ js/app.js
@@ -19,16 +19,39 @@ $(document).ready(function(){
                 $(this).css('left', '520px');
             });
     })
+
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-    });
+    
+    })
+    $('body').keydown(function(e) {
+        if (e.which ==88) {
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-cool').show();
+            $('.hadouken').hide();
+        }
+    })
 
+     $('body').keyup(function(e) {
+        if (e.which == 88) {
+            $('.ryu-still').show();
+            $('.ryu-ready').hide();
+            $('.ryu-cool').hide();
+            $('.hadouken').hide();
+        }
+    })
+   
+      
 });
+
 
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play();
-    });
+    }
+
+
 
